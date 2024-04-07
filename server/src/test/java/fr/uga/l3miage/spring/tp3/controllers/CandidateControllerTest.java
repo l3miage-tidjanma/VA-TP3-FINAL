8d883a250df84c4dde32776ab3da2ef697cf4417
tidjanma <Manyl.Tidjani@etu.univ-grenoble-alpes.fr>
2024-04-07 23:36:25 +0200

    - CandidateControllerTest ok

--- server/src/test/java/fr/uga/l3miage/spring/tp3/controllers/CandidateControllerTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp3/controllers/CandidateControllerTest.java
@@ -4,18 +4,20 @@ import fr.uga.l3miage.spring.tp3.exceptions.rest.CandidateNotFoundRestException;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
 import fr.uga.l3miage.spring.tp3.models.CandidateEvaluationGridEntity;
 import fr.uga.l3miage.spring.tp3.models.ExamEntity;
+import fr.uga.l3miage.spring.tp3.repositories.CandidateEvaluationGridRepository;
 import fr.uga.l3miage.spring.tp3.repositories.CandidateRepository;
 
-import fr.uga.l3miage.spring.tp3.responses.CandidateResponse;
-import org.junit.jupiter.api.AfterEach;
+import fr.uga.l3miage.spring.tp3.repositories.ExamRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.core.ParameterizedTypeReference;
 
 import javax.transaction.Transactional;
 import java.util.Map;
@@ -33,48 +35,67 @@ public class CandidateControllerTest {
     private TestRestTemplate testRestTemplate;
     @Autowired
     private CandidateRepository candidateRepository;
+    @Autowired
+    private CandidateEvaluationGridRepository candidateEvaluationGridRepository;
+
+    @MockBean
+    private ExamRepository examRepository;
+
 
     @BeforeEach
     @Transactional
     public void cleanBDsetUp() {
         candidateRepository.deleteAll();
+        candidateEvaluationGridRepository.deleteAll();
     }
 
     @Test
     void getAverageCandidateFound() {
         // Given //
+        // il faut enregistrer tes changements
         final HttpHeaders headers = new HttpHeaders();
 
         // création des examens
         ExamEntity examEntity1 = ExamEntity.builder().weight(1).build();
         ExamEntity examEntity2 = ExamEntity.builder().weight(2).build();
 
-        // création des grilles d'évaluation associées
-        CandidateEvaluationGridEntity candidateEvaluationGrid1 = CandidateEvaluationGridEntity
-                .builder()
-                .grade(10.5)
-                .examEntity(examEntity1)
-                .build();
-        CandidateEvaluationGridEntity candidateEvaluationGrid2 = CandidateEvaluationGridEntity
-                .builder()
-                .grade(15.25)
-                .examEntity(examEntity2)
-                .build();
+        // sauvegarde des examens dans la BD H2
+        examRepository.saveAll(Set.of(examEntity1,examEntity2));
+
 
         // création du candidat
         CandidateEntity candidateEntity = CandidateEntity
                 .builder()
-                .id(9L)
                 .email("rayane@yahoo.fr")
-                .candidateEvaluationGridEntities(Set.of(candidateEvaluationGrid1, candidateEvaluationGrid2))
                 .build();
 
         // sauvegarde du candidat dans la BD H2
         candidateRepository.save(candidateEntity);
 
+        // création des grilles d'évaluation associées
+        CandidateEvaluationGridEntity candidateEvaluationGrid1 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(10.5)
+                .examEntity(examEntity1)
+                .candidateEntity(candidateEntity)
+                .build();
+        CandidateEvaluationGridEntity candidateEvaluationGrid2 = CandidateEvaluationGridEntity
+                .builder()
+                .grade(15.25)
+                .examEntity(examEntity2)
+                .candidateEntity(candidateEntity)
+                .build();
+
+        // sauvegarder toutes les grilles d'évaluation dans la BD
+        candidateEvaluationGridRepository.saveAll(Set.of(candidateEvaluationGrid1,candidateEvaluationGrid2));
+        candidateEntity.setCandidateEvaluationGridEntities(Set.of(candidateEvaluationGrid1,candidateEvaluationGrid2));
+
+        candidateRepository.save(candidateEntity);
+
+
         // When //
         // récupération de la moyenne a travers l'api
-        ResponseEntity<Double> response = testRestTemplate.exchange("/api/candidates/{id}/average", HttpMethod.GET, new HttpEntity<>(null, headers), Double.class, Map.of("id",9));
+        ResponseEntity<Double> response = testRestTemplate.exchange("/api/candidates/{id}/average", HttpMethod.GET, new HttpEntity<>(null, headers), new ParameterizedTypeReference<>() {}, Map.of("id", candidateEntity.getId()));
 
         // Then //
         // vérifier que le code statut est OK (=200)
